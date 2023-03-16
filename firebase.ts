aa0e79b663a0aa6ed634d780d27bcdfff8e43b5f
JstVex <heinhtetlulu14@gmail.com>
2023-03-16 17:45:54 +0630

    Initial commit

---
+++ firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC6i_jMocw6IHD27kySpfdcLqoyttgDgDI",
+  authDomain: "mercury-b19e1.firebaseapp.com",
+  projectId: "mercury-b19e1",
+  storageBucket: "mercury-b19e1.appspot.com",
+  messagingSenderId: "611593705824",
+  appId: "1:611593705824:web:ef5d97a094477e953b6605"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
