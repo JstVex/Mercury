7297c40dded8a54a66fa64a57390e6f616ffcfe1
JstVex <heinhtetlulu14@gmail.com>
2023-03-18 01:44:03 +0630

    Changed navbar to sidebar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import Sidebar from '../../components/sidebar'
+import Sidebar from '../components/sidebar'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
